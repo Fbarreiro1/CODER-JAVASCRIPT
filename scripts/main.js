b039b7dbb679d93665685eafb30545dd7d49c712
Fbarreiro <fed_eh@hotmail.com>
2022-03-26 08:58:39 -0300

    primera entrega proyecto final

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
-// funcion que calcula el precio total de los libros. Si se compran más de 3, el 4to tiene descuento del 50%
-const calcularPrecio = (libro1 = 0,libro2 = 0,libro3 = 0,libro4 = 0) => {
-	if (libro4 != 0) {
-		libro4 = (libro4 * 50)/ 100; }
-	return precio = parseFloat(libro1 + libro2 + libro3 + libro4);
-}
 
-// calcula el precio total de los libros que compra el usuario. Acepta máximo 4 libros y hay descuento si hay al menos 2 del mismo género
-			let cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
-			let precioTotal = 0;
+// Se pide que el usuario ingrese la cantidad de libros que va a comprar. SI está entre 1 y 4 se le pide que ingrese los nombres de los libros y los precios. Cada libro y precio conforman un nuevo objeto Libro que ingresa al array cantLibros y además se los escribe en la pagina con document.write.
+
+class Libro {
+	constructor(nombre,precio) {
+	  this.nombre = nombre.toLowerCase();
+	  this.precio = parseFloat(precio);
+	}
+  }
+  const listaLibros = [];
+  
+  			let cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
+			
 			if (cantLibro == 0 || cantLibro < 0) {
 				alert("Por favor, ¡Compre algo!") }
 
@@ -15,18 +18,33 @@ const calcularPrecio = (libro1 = 0,libro2 = 0,libro3 = 0,libro4 = 0) => {
 				alert("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")	
 			
 			}
-			else {
-			let genero = prompt("¿Lleva 2 o más libros del mismo género?");
-			
-				for (let k = 1; k <= cantLibro; k++) {
-					let str = parseFloat(prompt("INGRESAR PRECIO DEL LIBRO " + k));
-					precioTotal += str;
-							} 
-			if(genero.toLowerCase() == "si") {
-				precioTotal -= (precioTotal *15)/100;
-			}
-				alert("El precio total es de " + precioTotal + " pesos");
-		};
 		
-		
-		
+else {
+do {
+    let compra = prompt("¿Qué libro lleva?");
+  let precio = parseFloat(prompt("Ingresar precio del libro "));
+	listaLibros.push(new Libro(compra,precio));
+	}
+	while(listaLibros.length < cantLibro);
+
+console.log(listaLibros)
+
+for (let libro of listaLibros) {
+  
+  document.write( "Libro:  " + libro.nombre + ". Precio: " + libro.precio + " pesos. <br>")
+}
+}
+// funcion que calcula el precio total de los libros. Si se compran más de 3, hay un descuento del 15% sobre el total
+total = 0;
+const calcularPrecio = arr => {
+  for (let l of arr) { 
+  total += l.precio; }
+  if (arr.length > 3) {
+	total = total - (total*15) /100;
+}
+  
+  return console.log("Total a pagar: " + total);
+ 	
+  }
+
+// calcularPrecio(listaLibros)
