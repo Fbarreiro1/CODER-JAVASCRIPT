e034397f099c36c8fb4b18ec475b1e27bb20762a
Fbarreiro <fed_eh@hotmail.com>
2022-04-09 11:16:25 -0300

    Segunda entrega proyecto final

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,59 @@
 /**
  * Si el usuario ingresa el nombre en la caja de texto se captura el input y se lo escribe en la página.
+ * Si ya hay datos guardados en Local Storage se pregunta si quiere usar esos datos. Si quiere se lo       * escribe en la página, si no quiere se inicia sin los datos y los tiene que cargar en la caja de texto.
  * Al clickear el botón subir compra se pide que el usuario ingrese la cantidad de libros que va a comprar. * Si está entre 1 y 4 se le pide que ingrese los nombres de los libros y los precios. 
  * Cada libro y precio conforman un nuevo objeto Libro que ingresa al array cantLibros 
  * Se arma una lista en html con los libros y precios usando DOM y eventos.
- * * Al clickear el botón calcular precio se calcula el total y se puede aplicar un descuento.
-*/
-let saludo = document.getElementById("saludo");
-saludo.addEventListener("input", e => {
-  let hola = saludo.value
-})
-let hola = saludo.addEventListener('keyup', e => {
-  if (e.keyCode === 13) {
-    let h3 = document.createElement("h3")
-    h3.innerHTML = "¡Hola " + saludo.value + "!";
-    document.getElementById("form").insertBefore(h3, saludo);
+ * * Si ya hay datos de libros guardados en local Storage se muestra un alert con los libros y precios
+ * Si el usuario quiere mantener esos datos confirma y listo, si no puede cargar los nuevos
+ */
 
+//////////////////////////7
+let user = localStorage.getItem("nombre");
+if (user == null || user == "undefined") {
+
+  let saludo = document.getElementById("saludo");
+  saludo.addEventListener("input", e => {
+    let usuario = saludo.value
+  })
+  let usuario = saludo.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+
+      let nombreH3 = document.createElement("h3")
+      nombreH3.innerHTML = "¡Hola " + saludo.value + "!";
+      document.getElementById("form").insertBefore(nombreH3, saludo);
+      localStorage.setItem("nombre", saludo.value);
+    }
+    localStorage.setItem("nombre", saludo.value);
+  })
+
+} else {
+  let nombreH3 = document.createElement("h3")
+  nombreH3.innerHTML = "¡Hola " + user + "!";
+  document.getElementById("form").insertBefore(nombreH3, saludo);
+  let cambiar = prompt("La última vez ingresaste como " + user + `\n` + "¿Querés cambiar tu nombre de usuario? s/n")
+
+  if (cambiar.toLowerCase() == "s" || cambiar.toLowerCase() == "si") {
+    nombreH3.remove()
+    let saludo = document.getElementById("saludo");
+    saludo.addEventListener("input", e => {
+      let usuario = saludo.value
+    })
+    let usuario = saludo.addEventListener('keyup', e => {
+      if (e.keyCode === 13) {
+        let nombreH3 = document.createElement("h3")
+        nombreH3.innerHTML = "¡Hola " + saludo.value + "!";
+        document.getElementById("form").insertBefore(nombreH3, saludo);
+        localStorage.setItem("nombre", saludo.value);
+      }
+    })
+  } else if (cambiar.toLowerCase() == "n" || cambiar.toLowerCase() == "no") {
+    alert("Ok, tu nombre de usuario es " + user)
+  } else {
+    alert("Por favor elegir si o no")
   }
-})
+}
+
 noCompra.onclick = function () {
   alert("Por favor, ¡Compre algo!");
 };
@@ -26,55 +63,81 @@ class Libro {
     this.precio = parseFloat(precio);
   }
 }
-const listaLibros = [];
-let cantLibro = 0;
-total = 0;
-
+let historial = localStorage.getItem("librosGuardados");
 compra.onclick = () => {
+  if (historial != null) {
+    array = JSON.parse(historial);
+    let salida = `La vez pasada usted guardó estos libros: \n`;
+    for (let i = 0; i < array.length; i++) {
+      salida = salida + (i + 1) + " -> " + array[i].nombre + " $ " + array[i].precio + `\n`;
+    }
+    alert(salida)
+    let pregunta = prompt("¿Quiere borrar los libros guardados y guardar nuevos? si/no")
+    if (pregunta.toLowerCase() == "s" || pregunta.toLowerCase() == "si") {
 
-  cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
+      const listaLibros = [];
+      let cantLibro = 0;
+      total = 0;
+      cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
 
-  if (cantLibro == 0 || cantLibro < 0) {
-    alert("Por favor, ¡Compre algo!")
-  } else if (cantLibro >= 5) {
-    alert("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")
+      if (cantLibro == 0 || cantLibro < 0) {
+        alert("Por favor, ¡Compre algo!")
+      } else if (cantLibro >= 5) {
+        alert("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")
 
+      } else {
+        localStorage.setItem("cantidad", cantLibro);
+        do {
+          let compra = prompt("¿Qué libro lleva?");
+          let precio = parseFloat(prompt("Ingresar precio del libro "));
+          listaLibros.push(new Libro(compra, precio));
+        }
+        while (listaLibros.length < cantLibro);
+
+        console.log(listaLibros)
+        localStorage.setItem("librosGuardados", JSON.stringify(listaLibros));
+
+
+        for (let libro of listaLibros) {
+          let li = document.createElement("li")
+          li.innerHTML = "Libro:  " + libro.nombre + ". Precio: " + libro.precio + " pesos. <br>";
+          document.getElementById("lista").appendChild(li)
+        }
+      }
+    } else if (pregunta.toLowerCase() == "n" || pregunta.toLowerCase() == "no") {
+      alert("¡Guardamos su elección!")
+      alert(salida)
+    } else {
+      alert("Por favor elija si o no")
+    }
   } else {
-    do {
-      let compra = prompt("¿Qué libro lleva?");
-      let precio = parseFloat(prompt("Ingresar precio del libro "));
-      listaLibros.push(new Libro(compra, precio));
+    const listaLibros = [];
+    let cantLibro = 0;
+    total = 0;
+    cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
+
+    if (cantLibro == 0 || cantLibro < 0) {
+      alert("Por favor, ¡Compre algo!")
+    } else if (cantLibro >= 5) {
+      alert("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")
+
+    } else {
+      do {
+        let compra = prompt("¿Qué libro lleva?");
+        let precio = parseFloat(prompt("Ingresar precio del libro "));
+        listaLibros.push(new Libro(compra, precio));
+      }
+      while (listaLibros.length < cantLibro);
+
+      console.log(listaLibros)
+      localStorage.setItem("librosGuardados", JSON.stringify(listaLibros));
+
+
+      for (let libro of listaLibros) {
+        let li = document.createElement("li")
+        li.innerHTML = "Libro:  " + libro.nombre + ". Precio: " + libro.precio + " pesos. <br>";
+        document.getElementById("lista").appendChild(li)
+      }
     }
-    while (listaLibros.length < cantLibro);
-
-    console.log(listaLibros)
-
-
-    for (let libro of listaLibros) {
-      let li = document.createElement("li")
-      li.innerHTML = "Libro:  " + libro.nombre + ". Precio: " + libro.precio + " pesos. <br>";
-      document.getElementById("lista").appendChild(li)
-    }
-
-  }
-
-};
-
-calcular.onclick = () => {
-  total = 0;
-  if (cantLibro <= 0 || cantLibro >= 5) {
-    alert("Recuerde completar correctamente su compra")
-  } else {
-
-    for (let l of listaLibros) {
-      total += l.precio;
-    }
-    if (listaLibros.length > 3) {
-      total = total - (total * 15) / 100;
-    }
-    let h2 = document.createElement("h2")
-    h2.innerHTML = "Total a pagar: " + total;
-    document.body.appendChild(h2);
-
   }
 }
