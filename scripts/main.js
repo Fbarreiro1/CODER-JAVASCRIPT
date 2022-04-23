12104e85f75377097f2cabd8ccf871854ed9e4a5
Fbarreiro <fed_eh@hotmail.com>
2022-04-23 15:08:42 -0300

    api and bootstrap

--- scripts/main.js
+++ scripts/main.js
@@ -1,116 +1,144 @@
 /**
- * Agregué Bootstrap y modifiqué el HTML
- * Uso Sweet Alert para estilizar las alertas
- * Si el usuario ingresa el nombre en la caja de texto se captura el input y se lo escribe en la página.
- * Si ya hay datos guardados en Local Storage se pregunta si quiere usar esos datos. Si quiere se lo       * escribe en la página, si no quiere se inicia sin los datos y los tiene que cargar en la caja de texto.
- * Al clickear el botón subir compra se pide que el usuario ingrese la cantidad de libros que va a comprar. * Si está entre 1 y 4 se le pide que ingrese los nombres de los libros y los precios. 
+ * Uso Bootstrap para estilizar el HTML
+  * Uso Sweet Alert para estilizar las alertas
+  * Recomendación semanal: Utilizo la Books API del New York Times para agarrar el libro top 1 de la semana y escribo en html titulo y autor
+ * Usuario y cambiar usuario: Si el usuario ingresa el nombre en la caja de texto se captura el input y se lo escribe en la página.
+ * Si ya hay datos guardados en Local Storage se pregunta si quiere usar esos datos. Si quiere se lo escribe en la página, si no quiere se inicia sin los datos y los tiene que cargar en la caja de texto.
+ * Agregar libros:  se pide que el usuario ingrese  los nombres de los libros y los precios. 
  * Cada libro y precio conforman un nuevo objeto Libro que ingresa al array cantLibros 
  * Se arma una lista en html con los libros y precios usando DOM y eventos.
- * * Si ya hay datos de libros guardados en local Storage se muestra un alert con los libros y precios
- * Si el usuario quiere mantener esos datos confirma y listo, si no puede cargar los nuevos
+ * Libros guardados: Si ya hay datos de libros guardados en local Storage se muestra un alert con los libros y precios.
+ * Si el usuario quiere mantener esos datos confirma y listo, si no puede cargar los nuevos.
  */
 
 //////////////////////////7
+let formNombre = document.getElementById("formNombre");
 let user = localStorage.getItem("nombre");
-if (user == null || user == "undefined") {
+let form = document.getElementById("formLibros");
+let nombreLibro = document.getElementById("nombreLibro");
+let precioLibro = document.getElementById("precioLibro");
+let lista = document.getElementById("listaLibros")
+let guardados = document.getElementById("guardados");
+const listaLibros = [];
+let cantLibro = 4;
+let historial = localStorage.getItem("librosGuardados");
+let btnRecomendar = document.getElementById("getBooks");
+let recomendacion = document.getElementById("recomendacion");
 
-  let saludo = document.getElementById("saludo");
-  saludo.addEventListener("input", e => {
-    let usuario = saludo.value
-  })
-  let usuario = saludo.addEventListener('keyup', e => {
-    if (e.keyCode === 13) {
-
-      let nombreH3 = document.createElement("h3")
-      nombreH3.innerHTML = "¡Hola " + saludo.value + "!";
-      document.getElementById("form").insertBefore(nombreH3, saludo);
-      localStorage.setItem("nombre", saludo.value);
-    }
-    localStorage.setItem("nombre", saludo.value);
-  })
-
-} else {
-  let nombreH3 = document.createElement("h3")
-  nombreH3.innerHTML = "¡Hola " + user + "!";
-  document.getElementById("form").insertBefore(nombreH3, saludo);
-  let cambiar = prompt("La última vez ingresaste como " + user + `\n` + "¿Querés cambiar tu nombre de usuario? s/n")
-
-  if (cambiar.toLowerCase() == "s" || cambiar.toLowerCase() == "si") {
-    nombreH3.remove()
-    let saludo = document.getElementById("saludo");
-    saludo.addEventListener("input", e => {
-      let usuario = saludo.value
-    })
-    let usuario = saludo.addEventListener('keyup', e => {
-      if (e.keyCode === 13) {
-        let nombreH3 = document.createElement("h3")
-        nombreH3.innerHTML = "¡Hola " + saludo.value + "!";
-        document.getElementById("form").insertBefore(nombreH3, saludo);
-        localStorage.setItem("nombre", saludo.value);
-      }
-    })
-  } else if (cambiar.toLowerCase() == "n" || cambiar.toLowerCase() == "no") {
-    Swal.fire("Ok, tu nombre de usuario es " + user)
-  } else {
-    Swal.fire("Por favor elegir si o no")
-  }
-}
-
-noCompra.onclick = function () {
-  Swal.fire("Por favor, ¡Compre algo!");
-};
 class Libro {
-  constructor(nombre, precio) {
-    this.nombre = nombre.toLowerCase();
+  constructor(compra, precio) {
+    this.compra = compra.toLowerCase();
     this.precio = parseFloat(precio);
   }
 }
-let historial = localStorage.getItem("librosGuardados");
-compra.onclick = () => {
-  if (historial != null) {
+
+if (user == null || user == "undefined") {
+  let nuevoDiv= document.createElement("div")
+      nuevoDiv.innerHTML = `<input type = "text" id="nombreUser" placeholder="nombre"/>
+	  <button> Enviar </button>`;
+      formNombre.append(nuevoDiv)
+  formNombre.addEventListener("submit", (e) => {
+  e.preventDefault();
+  localStorage.setItem("nombre", nombreUser.value);
+   let nuevoH1 = document.createElement("h1")
+   nuevoH1.innerHTML = `¡Hola ${nombreUser.value}!`;
+    nombreH1.append(nuevoH1)
+  })
+}
+else {
+  Swal.fire("¡Hola " + user + "!");
+  let nuevoH1 = document.createElement("h1")
+   nuevoH1.innerHTML = `¡Hola ${user}!`;
+    nombreH1.append(nuevoH1)
+  let cambiarUser = document.createElement("div")
+  cambiarUser.innerHTML = `<input class="form-control" type = "text" id="cambioUser" placeholder="Cambiar usuario"/>
+	  <button class="btn text-black btn-outline-primary mt-1"><h4> Cambiar</h4>  </button>`;
+   formNombre.append(cambiarUser)
+      
+  formNombre.addEventListener("submit", (e) => {
+  e.preventDefault();
+  localStorage.setItem("nombre", cambioUser.value);
+   let nuevoH1 = document.createElement("h1")
+   nuevoH1.innerHTML = `Hola ${cambioUser.value}`;
+    nombreH1.append(nuevoH1)
+    
+})
+}
+guardados.onclick = () => {
+    if (historial != null) {
     array = JSON.parse(historial);
     let salida = `La vez pasada usted guardó estos libros: \n`;
     for (let i = 0; i < array.length; i++) {
-      salida = salida + (i + 1) + ". " + array[i].nombre + " $" + array[i].precio + `\n`;
-    }
+      salida = salida + (i + 1) + ". " + array[i].compra + " $" + array[i].precio + `\n`;
+      Swal.fire(salida).then(() => {
+        Swal.fire({
+  title: "¿Quiere quedarse con los libros guardados o borrarlos y guardar nuevos?",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Quedarme con los libros guardados',
+  cancelButtonText: 'Borrarlos y guardar nuevos'
+}).then((result) => {
+  if (!result.isConfirmed) {
+    localStorage.removeItem("librosGuardados");
+    Swal.fire("Su historial de libros se ha borrado. Por favor cargue nuevos")
     
-    let pregunta = prompt("¿Quiere borrar los libros guardados y guardar nuevos? si/no")
-    if (pregunta.toLowerCase() == "s" || pregunta.toLowerCase() == "si") {
-      localStorage.removeItem("librosGuardados");
-      const listaLibros = [];
-      let cantLibro = 0;
-      total = 0;
-      cantLibro = parseInt(prompt("Ingrese cantidad de libros "));
-
-      if (cantLibro == 0 || cantLibro < 0) {
-        Swal.fire("Por favor, ¡Compre algo!")
-      } else if (cantLibro >= 5) {
-        Swal.fire("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")
-
-      } else {
-        localStorage.setItem("cantidad", cantLibro);
-        do {
-          let compra = prompt("¿Qué libro lleva?");
-          let precio = parseFloat(prompt("Ingresar precio del libro "));
-          listaLibros.push(new Libro(compra, precio));
-        }
-        while (listaLibros.length < cantLibro);
-
-        console.log(listaLibros)
-        localStorage.setItem("librosGuardados", JSON.stringify(listaLibros));
-
-
-        for (let libro of listaLibros) {
+  }
+  else if (result.isConfirmed) {
+    for (let libro of array) {
           let li = document.createElement("li")
-          li.innerHTML = "Libro:  " + libro.nombre + ". Precio: " + libro.precio + " pesos. <br>";
-          document.getElementById("lista").appendChild(li)
+          li.innerHTML = `<li class="list-group-item active"> Libro: ${libro.compra}. Precio: ${libro.precio} pesos. </li><br>`;
+          lista.appendChild(li)
         }
+    
+  }
+})
       }
-    } else if (pregunta.toLowerCase() == "n" || pregunta.toLowerCase() == "no") {
-      Swal.fire("¡Guardamos su elección!")
-      Swal.fire(salida)
-    } else {
-      Swal.fire("Por favor elija si o no")
+      
+    )}
+    
     }
-  } 
-}
+else {
+  Swal.fire("No tiene libros guardados. Agregue libros y recordaremos su elección ")
+}}
+    
+  btnRecomendar.addEventListener("click", () => {
+            let key = "ExykLl7MFfhTZSBq2QwA9dSGmOyks0Y5";
+             
+  let url = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=ExykLl7MFfhTZSBq2QwA9dSGmOyks0Y5"
+                fetch(url)
+                    .then((data) => {
+                        return data.json()
+                }).then((libros) => {
+                  let titulo = libros.results.lists[0].books[0].title;
+                  let autor = libros.results.lists[0].books[0].author;
+                  recomendacion.innerHTML = `<h3>Recomendación de la semana: <br> Libro: ${titulo} <br> Autor: ${autor}<h3>`;
+        
+            }).catch((err) => {
+        Swal.fire("Algo salió mal. Por favor pruebe en otro momento")
+      })
+  }) 
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (nombreLibro.value && precioLibro.value ) {
+    if (!isNaN(parseFloat(precioLibro.value))) {
+      if(listaLibros.length <4 ) {
+      listaLibros.push(new Libro(nombreLibro.value, precioLibro.value));
+      localStorage.setItem("librosGuardados", JSON.stringify(listaLibros));
+      let nuevaLi = document.createElement("li")
+      nuevaLi.innerHTML = `<li class="list-group-item active"> Libro: ${nombreLibro.value}. Precio: ${precioLibro.value} pesos. </li><br>`;
+      lista.append(nuevaLi)}
+      else {
+        Swal.fire("Recuerde que la máxima cantidad permitida en una sola compra es de 4 libros.")
+      }
+      
+    }
+    else {
+      Swal.fire("El precio debe ser un número")
+    }
+  } else {
+    Swal.fire("Complete ambos campos")
+  }
+})
